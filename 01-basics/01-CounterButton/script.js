4a50c940726bab32fedac5dad64f6f0de764d64d
Anastasiia Konstantinovich <na.konsta@gmail.com>
2020-10-01 21:10:10 +0300

    Counter button task is ready

--- 01-basics/01-CounterButton/script.js
+++ 01-basics/01-CounterButton/script.js
@@ -1,4 +1,18 @@
 import Vue from './vue.esm.browser.js';
 
-// const app = ...
+const app = new Vue({
+    el: '#app',
+
+    data() {
+        return {
+            buttonNum: 0
+        }
+    },
+
+    methods: {
+        increaseNum() {
+            this.buttonNum++;
+        }
+    }
+})
 // Рекомендуется использовать МЕТОД в качестве обработчика события
