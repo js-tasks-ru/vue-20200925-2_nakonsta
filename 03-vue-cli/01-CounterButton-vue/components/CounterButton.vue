579d2ac56c200b1b12c37e61e9bfeddb5f287217
Anastasiia Konstantinovich <na.konsta@gmail.com>
2020-10-10 12:36:23 +0300

    CounterButton as SFC

--- 03-vue-cli/01-CounterButton-vue/components/CounterButton.vue
+++ 03-vue-cli/01-CounterButton-vue/components/CounterButton.vue
@@ -1,7 +1,29 @@
-<template><div></div></template>
+<template>
+    <button @click="increaseCount()">{{ count }}</button>
+</template>
 
 <script>
-export default {};
+export default {
+    name: 'CounterButton',
+
+    props: {
+        count: {
+            type: Number,
+            default: 0,
+        }
+    },
+
+    model: {
+        prop: 'count',
+        event: 'increment',
+    },
+
+    methods: {
+        increaseCount() {
+            return this.$emit('increment', this.count + 1)
+        }
+    }
+};
 </script>
 
 <style></style>
